de02f59cae61a60bdd91af9d0a62b0ac2c8ade26
bumpMind <damian.s.ja@gmail.com>
2017-11-05 18:57:37 +0100

    Updated initial database SQL (contains simple books, authors, categories nad publishers)

--- docker/data/init_db_05112017.sql
+++ docker/data/init_db_05112017.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 03, 2017 at 06:39 PM
--- Server version: 5.5.54-38.6-log
+-- Generation Time: Nov 05, 2017 at 05:02 PM
+-- Server version: 5.7.18-14-log
 -- PHP Version: 7.1.7
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
@@ -40,7 +40,14 @@ CREATE TABLE IF NOT EXISTS `AddressData` (
   `city` varchar(45) NOT NULL,
   PRIMARY KEY (`idAddressData`),
   UNIQUE KEY `idAddressData_UNIQUE` (`idAddressData`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin2 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin2 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `AddressData`
+--
+
+INSERT INTO `AddressData` (`idAddressData`, `name`, `fisrtName`, `surname`, `nipNumber`, `phoneNumber`, `street`, `houseNumber`, `apartmentNumber`, `postalCode`, `city`) VALUES
+(1, 'name', 'firstName', 'surname', 'nipNumber', 918273652, 'street', 'houseNumber', 'apartmentNumber', 'postalCode', 'city');
 
 -- --------------------------------------------------------
 
@@ -54,19 +61,16 @@ CREATE TABLE IF NOT EXISTS `Author` (
   `surName` varchar(45) NOT NULL,
   PRIMARY KEY (`idAuthor`),
   UNIQUE KEY `idAutor_UNIQUE` (`idAuthor`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin2 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin2 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `Author`
 --
 
 INSERT INTO `Author` (`idAuthor`, `firstName`, `surName`) VALUES
-(1, 'firssst', 'surrrName'),
-(2, 'test1', 'muahahahah'),
-(3, 'test1', 'janusze zla'),
-(4, 'test1', 'test2'),
-(5, 'test1', 'test2'),
-(6, 'Jacek', 'Panek');
+(7, 'Dan', 'Brown'),
+(9, 'Stephen', 'King'),
+(11, 'Nesbo', 'Jo');
 
 -- --------------------------------------------------------
 
@@ -80,6 +84,16 @@ CREATE TABLE IF NOT EXISTS `AuthorBook` (
   PRIMARY KEY (`idAuthor`,`ISBN`),
   KEY `ISBN_idx` (`ISBN`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin2;
+
+--
+-- Dumping data for table `AuthorBook`
+--
+
+INSERT INTO `AuthorBook` (`idAuthor`, `ISBN`) VALUES
+(11, '9788324589913'),
+(11, '9788327157317'),
+(9, '9788378859369'),
+(7, '9788381101431');
 
 -- --------------------------------------------------------
 
@@ -104,6 +118,16 @@ CREATE TABLE IF NOT EXISTS `Book` (
   KEY `idPublisher_idx` (`idPublisher`),
   KEY `idCategory_idx` (`idCategory`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin2;
+
+--
+-- Dumping data for table `Book`
+--
+
+INSERT INTO `Book` (`ISBN`, `title`, `idPublisher`, `year`, `idCategory`, `description`, `quantity`, `onDemand`, `suspended`, `purchasePrice`, `sellingPrice`, `numberSold`) VALUES
+('9788324589913', 'Czerwone gardło', 3, 2010, 1, 'Wiosna 1940 roku. Wojna w pełnym toku, RAF zmaga się z Luftwaffe, młodzi mężczyźni poszli na front, a Tommy Beresford, bohater poprzedniej wojny, siedzi w domu i czuje się niepotrzebny. Na szczęście otrzymuje tajne zadanie: wytropienia siatki szpiegowskiej działającej w spokojnym angielskim kurorcie. Nie jest to łatwe zadanie. Będący na jej tropie najlepszy agent został zamordowany. Zostawił jednak wskazówkę: wroga należy szukać wśród gości hoteliku Sans Souci. ', 2, 0, 0, 14.79, 21.49, 28),
+('9788327157317', 'Pierwszy śnieg', 3, 2017, 1, 'Jest listopad, w Oslo właśnie spadł pierwszy śnieg. Birte Becker po powrocie z pracy do domu chwali syna i męża za ulepienie bałwana w ogrodzie. Nie jest on jednak ich dziełem. Stają przy oknie - i widzą, że bałwan jest skierowany twarzą w stronę domu. Patrzy wprost na nich.\r\nW tym samym czasie komisarz Harry Hole otrzymuje anonimowy list podpisany "Bałwan". Zaczyna dostrzegać wspólne cechy dawnych, niewyjaśnionych spraw. Okazuje się, że wraz z pierwszymi oznakami zimy do gazet trafia informacja o nowym morderstwie. Ofiara jest zawsze zamężną kobietą, a jednocześnie w pobliżu miejsca zbrodni pojawia się bałwan. Wszystko wskazuje na to, że po Oslo i okolicach znów krąży seryjny zabójca. ', 10, 0, 0, 15.99, 25.62, 0),
+('9788378859369', ' Pan Mercedes', 2, 2016, 1, 'Debiut Stephena Kinga w gatunku kryminału detektywistycznego hardboiled.\r\nTuż przed świtem, w spokojnym miasteczku, setki zdesperowanych, bezrobotnych ludzi stoi w kolejce na targi pracy. Nagle, bez ostrzeżenia, samotny kierowca w kradzionym Mercedesie wpada w tłum. Zabija osiem osób, rani piętnaście. Ucieka z miejsca wypadku. Brady, nazywający sam siebie "sprafcą", pokochał dotyk śmierci pod kołami Mercedesa i chce poczuć go ponownie. Kolejna misja Brady''ego, jeśli się powiedzie, zabije tysiące osób.\r\nTylko emerytowany detektyw Bill Hodges wraz z dwójką sprzymierzeńców może zatrzymać mordercę, zanim ten uderzy ponownie. Kto kryje się pod pseudonimem Pan Mercedes i jak go odnaleźć? ', 20, 0, 0, 14.79, 29.27, 15),
+('9788381101431', 'Poczatek', 1, 2017, 3, ' Robert Langdon, profesor Uniwersytetu Harvarda, specjalista w dziedzinie ikonologii religijnej i symboli, przybywa do Muzeum Guggenheima w Bilbao, gdzie ma dojść do ujawnienia odkrycia, które ?na zawsze zmieni oblicze nauki?. \r\nGospodarzem wieczoru jest Edmond Kirsch, czterdziestoletni miliarder i futurysta, którego oszałamiające wynalazki i śmiałe przepowiednie zapewniły mu rozgłos na całym świecie. Kirsch, który dwadzieścia lat wcześniej był jednym z pierwszych studentów Langdona na Harvardzie, planuje ujawnić informację, która będzie stanowić odpowiedź na fundamentalne pytania dotyczące ludzkiej egzystencji.\r\nGdy Langdon i kilkuset innych gości w osłupieniu ogląda oryginalną prezentację, wieczór zmienia się w chaos, a cenne odkrycie Kirscha może przepaść na zawsze. Chcąc stawić czoła nieuchronnemu zagrożeniu, Langdon musi uciekać z Bilbao. Towarzyszy mu Ambra Vidal, elegancka dyrektorka muzeum, która pomagała Kirschowi zorganizować wydarzenie. Razem udają się do Barcelony i podejmują niebezpieczną misję odnalezienia kryptograficznego hasła, które stanowi klucz do sekretu Kirscha. ', 12, 0, 0, 10.99, 26.99, 8);
 
 -- --------------------------------------------------------
 
@@ -131,7 +155,16 @@ CREATE TABLE IF NOT EXISTS `Category` (
   PRIMARY KEY (`idCategory`),
   UNIQUE KEY `idCategory_UNIQUE` (`idCategory`),
   UNIQUE KEY `name_UNIQUE` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin2 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin2 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `Category`
+--
+
+INSERT INTO `Category` (`idCategory`, `name`) VALUES
+(2, 'Horror'),
+(1, 'Kryminal'),
+(3, 'Sensacja');
 
 -- --------------------------------------------------------
 
@@ -226,7 +259,16 @@ CREATE TABLE IF NOT EXISTS `Publisher` (
   PRIMARY KEY (`idPublisher`),
   UNIQUE KEY `idPublisher_UNIQUE` (`idPublisher`),
   KEY `idAddressData_idx` (`idAddressData`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin2 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin2 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `Publisher`
+--
+
+INSERT INTO `Publisher` (`idPublisher`, `name`, `idAddressData`, `email`) VALUES
+(1, 'Sonia Draga', 1, 'sonia-draga@mail.com'),
+(2, 'ALBATROS', 1, 'albatros@mail.com'),
+(3, 'Dolnośląskie', 1, 'wyd-dolnoslaskie@mail.com');
 
 -- --------------------------------------------------------
 
@@ -255,8 +297,8 @@ CREATE TABLE IF NOT EXISTS `User` (
 -- Constraints for table `AuthorBook`
 --
 ALTER TABLE `AuthorBook`
-  ADD CONSTRAINT `idAuthor` FOREIGN KEY (`idAuthor`) REFERENCES `Author` (`idAuthor`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `ISBN` FOREIGN KEY (`ISBN`) REFERENCES `Book` (`ISBN`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `ISBN` FOREIGN KEY (`ISBN`) REFERENCES `Book` (`ISBN`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `idAuthor` FOREIGN KEY (`idAuthor`) REFERENCES `Author` (`idAuthor`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `Book`
@@ -269,8 +311,8 @@ ALTER TABLE `Book`
 -- Constraints for table `BookInOrder`
 --
 ALTER TABLE `BookInOrder`
-  ADD CONSTRAINT `idOrder` FOREIGN KEY (`idOrder`) REFERENCES `Order` (`idOrder`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `ISBNorder` FOREIGN KEY (`ISBN`) REFERENCES `Book` (`ISBN`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `ISBNorder` FOREIGN KEY (`ISBN`) REFERENCES `Book` (`ISBN`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `idOrder` FOREIGN KEY (`idOrder`) REFERENCES `Order` (`idOrder`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `Deliverer`
@@ -282,8 +324,8 @@ ALTER TABLE `Deliverer`
 -- Constraints for table `Delivery`
 --
 ALTER TABLE `Delivery`
-  ADD CONSTRAINT `idPublisherDelivery` FOREIGN KEY (`idDeliverer`) REFERENCES `Deliverer` (`idDeliverer`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `ISBNdelivery` FOREIGN KEY (`ISBN`) REFERENCES `Book` (`ISBN`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `ISBNdelivery` FOREIGN KEY (`ISBN`) REFERENCES `Book` (`ISBN`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `idPublisherDelivery` FOREIGN KEY (`idDeliverer`) REFERENCES `Deliverer` (`idDeliverer`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `Order`
@@ -307,8 +349,8 @@ ALTER TABLE `Publisher`
 -- Constraints for table `User`
 --
 ALTER TABLE `User`
-  ADD CONSTRAINT `idDeliveryData` FOREIGN KEY (`idDeliveryData`) REFERENCES `AddressData` (`idAddressData`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `idAddressDataUser` FOREIGN KEY (`idAddressData`) REFERENCES `AddressData` (`idAddressData`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `idDeliveryData` FOREIGN KEY (`idDeliveryData`) REFERENCES `AddressData` (`idAddressData`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `idInvoiceData` FOREIGN KEY (`idInvoiceData`) REFERENCES `AddressData` (`idAddressData`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
